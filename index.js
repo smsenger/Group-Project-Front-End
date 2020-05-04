c27b99474f1227d74a96bf155725c44482de10a6
Saesha Senger <saesha.senger@gmail.com>
2020-05-04 14:12:57 -0400

    adds three main files

---
+++ index.js
@@ -0,0 +1,17 @@
+affirmation: https://www.affirmations.dev/ 
+
+
+
+
+
+
+
+// window.onSpotifyWebPlaybackSDKReady = () => {
+//   const userAccessToken = "[access token]";
+//   const webPlayback = new Spotify.Player({
+//     name: "Spotify Web Playback SDK",
+//     getOAuthToken: callback => { callback(token)}
+//   });
+//   webPlayback.connect();
+// };
+// </script>

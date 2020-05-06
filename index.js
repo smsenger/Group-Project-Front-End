33fd171aaf61bd408c9a285fc062d33e64a167ef
KelseyCortez <kelscortez@gmail.com>
2020-05-06 10:24:50 -0400

    adds filter to author

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const settings = {
   "method": "GET"
 }
 
+let quotes = []
+
 
 function getRandomIndex(max) {
   return Math.floor(Math.random() * Math.floor(max));
@@ -17,26 +19,25 @@ function getRandomIndex(max) {
 
 function getSpace() {
   $.get('https://api.nasa.gov/planetary/apod?api_key=yhAGByOoN03Qq1qHf4S1IBX070g1Of5dMIrzjLdf')
-    .then(function(data2) {
+    .then(function (data2) {
       const img = document.getElementsByTagName('img')[0];
       console.log('data =', data2)
       img.setAttribute('src', data2.url)
-      let random = data[getRandomIndex(data.length)]
 
-    }); 
-  }; 
-  getSpace()
+    });
+};
+
 
 
 function getCats() {
   $.get('http://aws.random.cat/meow')
-    .then(function(data2) {
+    .then(function (data2) {
       const img = document.getElementsByTagName('img')[0];
       console.log('data =', data2)
       img.setAttribute('src', data2.file)
-    }); 
-  }; 
-  // getCats()
+    });
+};
+// getCats()
 
 
 
@@ -47,35 +48,42 @@ function renderQuote(quoteData) {
   }
   else {
     $('#author').text(quoteData.author)
-    
+
   };
   console.log(quoteData)
 }
 
 
 // $(function () {
-  const getFoxyQuote = function() {
-    $('#click-quote').html('Get me another!')   
-    $.get(settings).then(function (response) {
-      let data = JSON.parse(response);
-      let random = data[getRandomIndex(data.length)]
-      renderQuote(random);
-      
-    });
-  }
+const getFoxyQuote = function () {
+  $('#click-quote').html('Get me another!')
+  let random = quotes[getRandomIndex(quotes.length)]
+  renderQuote(random);
+
+}
 
 function startSong() {
   document.querySelectorAll('button')[0].click();
   document.querySelectorAll('button')[1].click();
-  }
+}
+
+$(document).ready( () => {
+  $.get(settings).then(function (response) {
+    const data = JSON.parse(response);
+    const removeTrump = data.filter(settingObj => settingObj.author != 'Donald Trump');
+    console.log(data);
+    quotes = removeTrump
+    console.log(quotes);
+
+  });
+  getSpace()
   
-
-
   $('#click-quote').click(function () {
     getFoxyQuote()
     getCats();
     startSong();
   });
+})
 
 
 // });
@@ -96,7 +104,7 @@ function startSong() {
 //        $("#affirmation").html("<em>\""+json.value.joke+"\"</em>").addClass("animated bounceIn");
 //      });
 //       });
-  
+
 //     });
 
 

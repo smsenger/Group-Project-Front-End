9132c265f72410b71b684c4f0b6ecc3d71eacfab
Chris Song <chris.bk.song@gmail.com>
2020-05-07 13:58:25 -0400

    updates layout on badvibes p

--- bad-vibes.js
+++ bad-vibes.js
@@ -1,15 +1,28 @@
-const randomImage = document.querySelector('.random-images');
+// const randomImage = document.querySelector('.random-images');
 
-document.addEventListener('DOMContentLoaded', function() {
+// document.addEventListener('DOMContentLoaded', function() {
 
+//   $.get('https://api.unsplash.com/search/photos?page=1&per_page=30&query=sad&client_id=yE582Cx8y-xmBPW0X_zUDPFAaJ0KYPUQqmVOZ1cgEfY')
+//     .then(function(data3) {
+//       const generatedImage = document.createElement('img');
+//       console.log('data =', data3)
+//       setInterval(function() {
+//         randomImage.innerHTML = '';
+//         generatedImage.setAttribute('src', data3.results[Math.floor(Math.random() * Math.floor(data3.results.length))].urls.small)
+//         randomImage.appendChild(generatedImage)
+//       }, 3000);
+//     }); 
+// });
+
+function getSadImages() {
   $.get('https://api.unsplash.com/search/photos?page=1&per_page=30&query=sad&client_id=yE582Cx8y-xmBPW0X_zUDPFAaJ0KYPUQqmVOZ1cgEfY')
-    .then(function(data3) {
-      const generatedImage = document.createElement('img');
+    .then(function (data3) {
+      const img = document.getElementsByTagName('img')[1];
       console.log('data =', data3)
-      setInterval(function() {
-        randomImage.innerHTML = '';
-        generatedImage.setAttribute('src', data3.results[Math.floor(Math.random() * Math.floor(data3.results.length))].urls.small)
-        randomImage.appendChild(generatedImage)
-      }, 2000);
-    }); 
+      img.setAttribute('src', data3.results[Math.floor(Math.random() * Math.floor(data3.results.length))].urls.small)
+    });
+};
+
+$('#click-quote').click(function () {
+  getSadImages();
 });
